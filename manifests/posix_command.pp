4c0805493f9a8a77056a829c3a0653c445fed5e5
root <root@master.(none)>
2013-11-27 09:01:40 +0000

    initial commit

---
+++ manifests/posix_command.pp
@@ -0,0 +1,41 @@
+# Simply installs the Mcollective agent on the master and nodes.
+
+define runyer::posix_command (
+  $command,        # command to run
+  $action_name = $title, #no spaces allowed
+  $description = "Runs ${command} on posix agents"
+  ) {
+
+  $ddl_file = template('runyer/posix_ddl.erb')
+  $rb_file  = template('runyer/posix_rb.erb')
+
+  # For the Unix/Linux agents and the Puppet Enterprise Master server
+  if $::kernel != 'windows' {
+
+    file { "/opt/puppet/libexec/mcollective/mcollective/agent/${title}.ddl":
+      ensure  => file,
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      content => $ddl_file,
+      notify  => Service['pe-mcollective'],
+    }
+
+    file { "/opt/puppet/libexec/mcollective/mcollective/agent/${title}.rb":
+      ensure  => file,
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      content => $rb_file,
+      notify  => Service['pe-mcollective'],
+    }
+
+  }
+
+  else {
+
+    notify { 'runyer::posix_command only supported on Linux master and Unix/Linux agent nodes': }
+
+  }
+
+}
---
+++ manifests/windows_command.pp
@@ -0,0 +1,66 @@
+# Simply installs the Mcollective agent on the master and nodes.
+
+define runyer::windows_command (
+  $command,        # command to run
+  $action_name = $title, #no spaces allowed
+  $description = "Runs ${command} on windows agents"
+  ) {
+
+  $ddl_file = template('runyer/win_ddl.erb')
+  $rb_file  = template('runyer/win_rb.erb')
+
+  if $::kernel == 'windows' {
+
+    file { "C:/ProgramData/PuppetLabs/mcollective/etc/plugins/mcollective/agent/${title}.ddl":
+      ensure  => file,
+      owner   => Administrator,
+      group   => Administrators,
+      mode    => '0644',
+      content => $ddl_file,
+      notify  => Service['pe-mcollective'],
+    }
+
+    file { "C:/ProgramData/PuppetLabs/mcollective/etc/plugins/mcollective/agent/${title}.rb":
+      ensure  => file,
+      owner   => Administrator,
+      group   => Administrators,
+      mode    => '0644',
+      content => $rb_file,
+      notify  => Service['pe-mcollective'],
+    }
+
+  }
+
+  # For the Puppet Enterprise Master server
+  elsif $::kernel == 'Linux' and
+    ($::puppet_server == $::fqdn or
+    $::puppet_server == $::hostname)
+  {
+
+    file { "/opt/puppet/libexec/mcollective/mcollective/agent/${title}.ddl":
+      ensure  => file,
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      content => $ddl_file,
+      notify  => Service['pe-mcollective'],
+    }
+
+    file { "/opt/puppet/libexec/mcollective/mcollective/agent/${title}.rb":
+      ensure  => file,
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      content => $rb_file,
+      notify  => Service['pe-mcollective'],
+    }
+
+  }
+
+  else {
+
+    notify { 'runyer::windows_command only supported on Linux master and Windows agent nodes': }
+
+  }
+
+}
