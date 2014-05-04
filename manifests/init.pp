965b17f6af96fbb41be0f3599adc2a221799050d
Jeremy Adams <jeremy@puppetlabs.com>
2014-05-03 18:45:21 -0700

    fixed commas

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
 class runyer (
-    $author_name  = 'anonymous'
-    $author_email = 'anonym@us'
-    $license      = 'Apache v2'
-    $version      = '1.0'
-    $project_url  = 'http://www.puppetlabs.com'
-    $timeout      = 15
+    $author_name  = 'anonymous',
+    $author_email = 'anonym@us',
+    $license      = 'Apache v2',
+    $version      = '1.0',
+    $project_url  = 'http://www.puppetlabs.com',
+    $timeout      = 15,
   ) { }
